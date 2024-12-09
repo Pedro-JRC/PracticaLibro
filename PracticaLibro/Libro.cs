8c302eac00c6c121a18673f52e3cd5ee089c0513
Pedro J. Rosario <131802637+Pedro-JRC@users.noreply.github.com>
2024-12-08 23:34:14 -0400

    Add project files.

---
+++ PracticaLibro/Libro.cs
@@ -0,0 +1,33 @@
+﻿
+namespace PracticaLibro
+{
+    public class Libro
+    {
+        // PROPIEDADES
+        public string Titulo {  get; set; }
+        public string Autor {  get; set; }
+        public int Paginas {  get; set; }
+
+        // CONSTRUCTOR QUE INICIA LAS PROPIEDADES
+        public Libro (string titulo, string autor, int paginas)
+        {
+            Titulo = titulo;
+            Autor = autor;
+            Paginas = paginas;
+        }
+
+        // METODO PARA MOSTRAR LOS DATOS DEL LIBRO
+        public void mostrarInformacion()
+        {
+            Console.WriteLine($"Titulo: {Titulo}");
+            Console.WriteLine($"Autor: {Autor}");
+            Console.WriteLine($"Paginas: {Paginas}");
+        }
+
+        // METODO PARA VERIFICAR LARGO DEL LIBRO
+        public bool esLargo()
+        {
+            return Paginas > 500;
+        }
+    }
+}
---
+++ PracticaLibro/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace PracticaLibro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // VARIABLES
+                string titulo;
+                string autor;
+                string paginasDigita;
+                int paginas;
+
+                // SOLICITA EL TITULO
+                Console.Write("Favor ingrese el titulo del libro: ");
+                titulo = Console.ReadLine();
+
+                // SOLICITA EL AUTOR
+                Console.Write("Favor ingrese el autor: ");
+                autor = Console.ReadLine();
+
+                // SOLICITA LA CANTIDAD DE PAGINAS
+                Console.Write("Favor indique la cantidad de paginas: ");
+                paginasDigita = Console.ReadLine();
+
+
+                // VALIDACIONES
+                if (int.TryParse(paginasDigita, out paginas))
+                {
+                    // OBJETO LIBRO
+                    Libro libro = new Libro(titulo, autor, paginas);
+
+                    // MUETSRA LA INFORMACION DEL LIBRO
+                    Console.WriteLine("\nInformacion del libro:");
+                    libro.mostrarInformacion();
+
+                    // INFORMACION DEL LARGO DEL LIBRO
+                    Console.WriteLine("\nLargo?:");
+
+                    // VERIFICA SI EL LIBRO ES LARGO
+                    if (libro.esLargo())
+                    {
+                        Console.WriteLine("El libro es largo.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("El libro no es largo.");
+                    }
+
+
+
+                }
+                else
+                {
+                    // MENSAE DE ERROR SI SE DIGITA UN DATO INVALIDO
+                    Console.WriteLine("ERROR: Favor ingrese un numero entero.");
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR: {ex.Message}.");
+            }
+        }
+    }
+}
